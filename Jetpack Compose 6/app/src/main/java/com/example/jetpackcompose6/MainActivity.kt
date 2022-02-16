5ba4a0c0e1b2d354453811e89be2595b140184a9
dwiwahyueffendi <dwiwahyueffendi22@gmail.com>
2022-02-16 12:12:35 +0700

    State

---
+++ Jetpack Compose 6/app/src/main/java/com/example/jetpackcompose6/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.jetpackcompose6
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScaffoldExample()
+        }
+    }
+}
+
+@Composable
+fun ScaffoldExample(){
+    val materialBlue700 = Color(0xFF1976D2)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Scaffold Tutorial")
+                },
+                 backgroundColor = materialBlue700
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                backgroundColor = materialBlue700
+            ){
+                Text(text = "Bottom Bar")
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Text(text = "+")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        drawerContent = {
+            Text("Ini Drawer")
+        },
+        content = {
+            Text("Ini Content")
+        }
+    )
+}
